26d20ace29f74ca0de3db9b68892cf03b5b59d0a
Andreas Lydersen <andreas.lydersen@prediktor.com>
2021-11-03 09:29:33 +0100

    Allowing files

--- setup.py
+++ setup.py
@@ -6,13 +6,8 @@ setup(
         "pythonnet>=2.5.2"
         ],
 
-    package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        "": ["*.txt", "*.rst", "*.md", "*.xml", "*.xsd"],
-        # And include any *.msg files found in the "hello" package, too:
-        # "hello": ["*.msg"],
-    },
-    version='0.1.4',
+    include_package_data=True,
+    version='0.1.5',
     description='A Python library to talk to Prediktor APIS/EDGE',
     author='Prediktor',
     license='MIT',
